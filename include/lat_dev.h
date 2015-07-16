cea2aa361d6c85e3402ac96b40d2e6e4f43c6b65
Geoffroy Vallee <geoffroy.vallee@free.fr>
2015-07-16 11:23:08 -0400

    rename header files to match new naming scheme.

---
+++ include/lat_dev.h
@@ -0,0 +1,22 @@
+/* vim: set softtabstop=4:shiftwidth=4:expandtab */
+
+/*
+ * Copyright (c) 2015       UT-Battelle, LLC
+ *                          All rights reserved.
+ *
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ *
+ */
+
+#ifndef LIBANALYZETHIS_DEV_H
+#define LIBANALYZETHIS_DEV_H
+
+typedef void* libanalyzethis_device_t;
+
+typedef void* libanalyzethis_device_sched_t;
+
+#endif /* LIBANALYZETHIS_DEV_H */
---
+++ include/lat_task.h
@@ -0,0 +1,20 @@
+/* vim: set softtabstop=4:shiftwidth=4:expandtab */
+
+/*
+ * Copyright (c) 2015       UT-Battelle, LLC
+ *                          All rights reserved.
+ *
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ *
+ */
+
+#ifndef LIBANALYZETHIS_TASK_H
+#define LIBANALYZETHIS_TASK_H
+
+typedef void* libanalyzethis_task_t;
+
+#endif /* LIBANALYZETHIS_TASK_H */
