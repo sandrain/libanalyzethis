529bb2cfec4b96ca95cccfc171f4bc750cdcd142
Geoffroy Vallee <geoffroy.vallee@free.fr>
2015-08-17 09:45:54 -0400

    Add a simple test for the no-op static placement policy.

---
+++ tests/noop_static_placement.py
@@ -0,0 +1,33 @@
+
+import sys, os.path
+
+cwd = os.path.abspath(os.path.curdir)
+abspath = os.path.join(cwd, '../src')
+print "Src path: %s\n" % abspath
+sys.path.append(abspath);
+
+import py_lat_module
+
+print "Calling lat_meta_sched_init()..."
+rc = py_lat_module.lat_meta_sched_init()
+if (rc != 0):
+    print "ERROR: lat_meta_sched_init() failed (ret: %d)\n" % rc
+print "Success.\n"
+
+# At the moment, we assume that the py_lat_module will always give us the noop
+# function for static placement (we do not have the plugin mechanism in place
+# yet).
+print "Calling the no-op static placement function. No task will be placed, will get the same file as the one passed as input"
+(rc, output_file) = py_lat_module.lat_meta_sched_workflow ("cybershake_60")
+if (rc != 0):
+    print "ERROR: lat_meta_sched_workflow() failed (%d)\n" % rc
+else:
+    print "Success.\n"
+
+print "Calling lat_meta_sched_finalize()..."
+rc = py_lat_module.lat_meta_sched_finalize()
+if (rc != 0):
+    print "ERROR: lat_meta_sched_finalize() failed (ret: %d)\n" % rc
+else:
+    print "Success.\n"
+
